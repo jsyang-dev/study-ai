ff5f4e0703a01ae23ba51c3408476a70e5cdc4e5
Jeongsu Yang <mycat83@gmail.com>
2025-03-08 14:49:36 +0900

    Initial commit

---
+++ src/main/java/me/study/ai/StudyAiApplication.java
@@ -0,0 +1,13 @@
+package me.study.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/study/ai/StudyAiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
