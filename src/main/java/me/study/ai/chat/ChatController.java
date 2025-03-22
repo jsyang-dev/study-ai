57d93e46d1b6e1bd38aab1ab52fb53a43b5c9d14
Jeongsu Yang <mycat83@gmail.com>
2025-03-22 16:56:08 +0900

    feat: Chat Client API - 기본 사용

---
+++ src/main/java/me/study/ai/chat/ChatController.java
@@ -0,0 +1,25 @@
+package me.study.ai.chat;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/chat")
+public class ChatController {
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @GetMapping("/simple")
+    public String simpleUserMessage(@RequestParam String input) {
+        return chatClient.prompt()
+                .user(input)
+                .call()
+                .content();
+    }
+}
