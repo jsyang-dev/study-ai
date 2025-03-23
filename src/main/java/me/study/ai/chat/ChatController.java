ced2f3c91aa5c4e48c2de3f05a76913d477f8174
Jeongsu Yang <mycat83@gmail.com>
2025-03-23 17:45:04 +0900

    feat: Chat Client API - 스트리밍

--- src/main/java/me/study/ai/chat/ChatController.java
+++ src/main/java/me/study/ai/chat/ChatController.java
@@ -1,13 +1,16 @@
 package me.study.ai.chat;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.converter.BeanOutputConverter;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/chat")
@@ -40,5 +43,23 @@ public class ChatController {
                 .user("Generate the filmography of 5 movies for Tom Hanks and Bill Murray.")
                 .call()
                 .entity(new ParameterizedTypeReference<>() {});
+    }
+
+    @GetMapping("/stream")
+    public List<ActorFilms> stream() {
+        BeanOutputConverter<List<ActorFilms>> converter = new BeanOutputConverter<>(new ParameterizedTypeReference<>() {});
+
+        Flux<String> flux = chatClient.prompt()
+                .user(u -> u.text("""
+                        Generate the filmography for a random 2 actors.
+                        {format}
+                      """)
+                        .param("format", converter.getFormat()))
+                .stream()
+                .content();
+
+        String content = flux.collectList().block().stream().collect(Collectors.joining());
+
+        return converter.convert(content);
     }
 }
