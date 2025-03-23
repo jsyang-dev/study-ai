a53f96e2b5c246ac8d206be1eefb2be4783076e9
Jeongsu Yang <mycat83@gmail.com>
2025-03-23 17:10:01 +0900

    feat: Chat Client API - 엔티티 목록 반환

--- src/main/java/me/study/ai/chat/ChatController.java
+++ src/main/java/me/study/ai/chat/ChatController.java
@@ -1,10 +1,13 @@
 package me.study.ai.chat;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/chat")
@@ -29,5 +32,13 @@ public class ChatController {
                 .user("Generate the filmography for a random actor.")
                 .call()
                 .entity(ActorFilms.class);
+    }
+
+    @GetMapping("/entities")
+    public List<ActorFilms> entities() {
+        return chatClient.prompt()
+                .user("Generate the filmography of 5 movies for Tom Hanks and Bill Murray.")
+                .call()
+                .entity(new ParameterizedTypeReference<>() {});
     }
 }
