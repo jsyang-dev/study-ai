b7be9738f7037f1ffac36b8de3c6dc93210d1fa7
Jeongsu Yang <mycat83@gmail.com>
2025-03-23 16:58:36 +0900

    build: Spring AI 버전 변경에 따른 수정

--- src/main/java/me/study/ai/chat/ChatController.java
+++ src/main/java/me/study/ai/chat/ChatController.java
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatController {
     private final ChatClient chatClient;
 
-    public ChatController(ChatClient.Builder chatClientBuilder) {
-        this.chatClient = chatClientBuilder.build();
+    public ChatController(ChatClient chatClient) {
+        this.chatClient = chatClient;
     }
 
     @GetMapping("/ai")
---
+++ src/main/java/me/study/ai/config/ChatClintConfig.java
@@ -0,0 +1,31 @@
+package me.study.ai.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.api.OpenAiApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatClintConfig {
+    @Value("${spring.ai.openai.api-key}")
+    private String apiKey;
+
+    @Bean
+    public ChatModel chatModel() {
+        OpenAiApi openAiApi = OpenAiApi.builder()
+                .apiKey(apiKey)
+                .build();
+
+        return OpenAiChatModel.builder()
+                .openAiApi(openAiApi)
+                .build();
+    }
+
+    @Bean
+    public ChatClient chatClient(ChatModel chatModel) {
+        return ChatClient.builder(chatModel).build();
+    }
+}
